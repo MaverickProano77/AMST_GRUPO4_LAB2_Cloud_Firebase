cee77ef0acec68884bb8118eef12d76ac84f4e8b
paula solorzano <pgsolorz@espol.edu.ec>
2022-10-29 00:35:51 -0500

    F

--- app/src/main/java/com/example/amst_grupo4_lab2_cloud_firebase/MainActivity.java
+++ app/src/main/java/com/example/amst_grupo4_lab2_cloud_firebase/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.amst_grupo4_lab2_cloud_firebase;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+         mAuth = FirebaseAuth.getInstance();
+         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.D
+                EFAULT_SIGN_IN)
+         .requestIdToken(getString(R.string.default_web_client_id))
+         .requestEmail()
+         .build();
+         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+         }
+
+    public void iniciarSesion(View view) {
+         resultLauncher.launch(new Intent(mGoogleSignInClient.getSignInIntent()));
+         }
+    ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new
+            ActivityResultContracts.StartActivityForResult(), new
+            ActivityResultCallback<ActivityResult>() {
+ @Override
+ public void onActivityResult(ActivityResult result) {
+                     if (result.getResultCode() == Activity.RESULT_OK) {
+                         Intent intent = result.getData();
+                         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(
+                                intent);
+                         try {
+                             GoogleSignInAccount account = task.getResult(ApiException.class);
+                             if (account != null) firebaseAuthWithGoogle(account);
+                             } catch (ApiException e) {
+                             Log.w("TAG", "Fallo el inicio de sesión con google.", e);
+                             }
+                         }
+                     }
+ });
 }
